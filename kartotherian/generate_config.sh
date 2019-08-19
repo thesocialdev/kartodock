0e5cf4bec11cd556e13a21ea8082c5758dd86f71
Mateus Santos <msantos@wikimedia.org>
2019-08-19 13:34:17 -0300

    Setup Kartotherian docker container
    1) Setup container with the proper dependencies
    2) Introduce generate_config script to create the proper config for
    kartotherian and tilerator
    3) Delete docker-compose.host.yml
    4) Remove start.sh since kartotherian now uses lerna and is much easier
    to handle linked packages

---
+++ kartotherian/generate_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setup config and sources yaml templates
+# Create Docker Config File from template
+if [ ! -f /etc/opt/config.docker.yaml ]; then
+	echo "Generating config.yaml"
+	perl -pe 's/\$([_A-Z]+)/$ENV{$1}/g' -i /etc/opt/config.kartotherian.docker.template.yaml \
+		&& cp /etc/opt/config.kartotherian.docker.template.yaml /etc/opt/config.kartotherian.docker.yaml
+	perl -pe 's/\$([_A-Z]+)/$ENV{$1}/g' -i /etc/opt/config.tilerator.docker.template.yaml \
+		&& cp /etc/opt/config.tilerator.docker.template.yaml /etc/opt/config.tilerator.docker.yaml
+fi
+
+echo "Generation of config files completed"
