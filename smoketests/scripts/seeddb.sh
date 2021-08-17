62a6b77614763688bcb1a1f5f1c87d95c088288b
mbsantos <msantos@wikimedia.org>
2021-08-17 18:27:21 +0200

    1) add tests for geoshapes query on Lago di Garda 2) Add github action to execute test image 3) Add water table in the imposm  mapping

--- smoketests/scripts/seeddb.sh
+++ smoketests/scripts/seeddb.sh
@@ -5,15 +5,15 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-imposm import -mapping /etc/app/kartodock/workspace/mapping.yml \
-    -read /etc/app/kartodock/smoketests/tests/fixtures/lago_di_garda.osm.pbf  \
+imposm import -mapping /etc/kartodock/workspace/mapping.yml \
+    -read /etc/kartodock/smoketests/tests/fixtures/lago_di_garda.osm.pbf  \
     -write \
     -connection "postgis: dbname=gis host=/var/run/postgresql/ prefix=NONE"
 
-imposm import -mapping /etc/app/kartodock/workspace/mapping.yml \
+imposm import -mapping /etc/kartodock/workspace/mapping.yml \
     -deployproduction \
     -connection "postgis: dbname=gis host=/var/run/postgresql/ prefix=NONE"
 
-pg_prove /etc/app/kartodock/smoketests/tests/osmWikidata.sql
+pg_prove /etc/kartodock/smoketests/tests/osmWikidata.sql
 
 exit 0
--- smoketests/tests/osmWikidata.sql
+++ smoketests/tests/osmWikidata.sql
@@ -1,7 +1,23 @@
 BEGIN;
-SELECT plan(1);
+SELECT plan(2);
 
 SELECT has_table('wikidata_relation_members');
+
+PREPARE geoshapes_query AS(
+    SELECT id, ST_AsGeoJSON(ST_Transform(ST_Simplify(geometry, 0.001*sqrt(ST_Area(ST_Envelope(geometry)))), 4326)) as data
+    FROM (
+        SELECT id, ST_Multi(ST_Union(geometry)) AS geometry
+        FROM (
+            SELECT wikidata AS id, (ST_Dump(geometry)).geom AS geometry
+            FROM wikidata_relation_members
+            WHERE wikidata IN ($$Q6414$$)
+        ) combq GROUP BY id
+    ) as subq);
+
+SELECT isnt_empty(
+    'geoshapes_query',
+    'Lago di Garda is imported and accessible to geoshapes query'
+);
 
 SELECT * FROM finish();
 ROLLBACK;
