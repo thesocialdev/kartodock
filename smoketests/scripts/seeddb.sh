87b10e7eebe891fbfee413b453bf3e999ba2fe11
mbsantos <msantos@wikimedia.org>
2021-08-19 12:09:35 +0200

    Move common config to maps-config and osm-bright.tm2source

--- smoketests/scripts/seeddb.sh
+++ smoketests/scripts/seeddb.sh
@@ -5,11 +5,8 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-imposm import -mapping /etc/kartodock/common/mapping.yml \
+imposm import -mapping /etc/kartodock/maps-config/osm-bright.tm2source/imposm_mapping.yml \
     -read /etc/kartodock/smoketests/tests/fixtures/lago_di_garda.osm.pbf  \
     -write \
-    -connection "postgis: dbname=gis host=/var/run/postgresql/ prefix=NONE"
-
-imposm import -mapping /etc/kartodock/common/mapping.yml \
-    -deployproduction \
-    -connection "postgis: dbname=gis host=/var/run/postgresql/ prefix=NONE"
+    -connection "postgis: dbname=gis host=/var/run/postgresql/ prefix=NONE" \
+    -deployproduction
